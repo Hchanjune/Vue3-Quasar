4f1c7d9f166dc0c78f5d791abb92a96f77494ff6
Hchanjune <99157556+Hchanjune@users.noreply.github.com>
2024-09-10 14:35:36 +0900

    initial commit

---
+++ src/pages/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import {ref} from "vue";
+
+  const mainText = ref("Hello World");
+
+  const changeText = (inputText: string) => {
+    mainText.value = inputText;
+  }
+
+</script>
+
+<template>
+  <div>
+    <p>{{mainText}}</p>
+    <input v-model="mainText" type="text" />
+    <button @click="changeText(mainText)">
+      텍스트 바꾸기
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
