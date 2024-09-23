20868146e343578c3ce1c96f90f5e20149ce82a8
Hchanjune <99157556+Hchanjune@users.noreply.github.com>
2024-09-23 13:39:18 +0900

    Update HelloWorld.vue

--- src/pages/HelloWorld.vue
+++ src/pages/HelloWorld.vue
@@ -2,7 +2,7 @@
 <!-- TEST -->
 
 <script setup lang="ts">
-  import {ref} from "vue";
+import { computed, ref } from 'vue'
 
   const mainText = ref("Hello World");
 
@@ -10,15 +10,20 @@
     mainText.value = inputText;
   }
 
+  const textLength = computed(() => {
+    return mainText.value.length;
+  });
+
 </script>
 
 <template>
   <div>
-    <p>{{mainText}}</p>
+    <p>{{ mainText }}</p>
     <input v-model="mainText" type="text" />
     <button @click="changeText(mainText)">
       텍스트 바꾸기
     </button>
+    <p>Text length: {{ textLength }}</p> <!-- New computed property -->
   </div>
 </template>
 
